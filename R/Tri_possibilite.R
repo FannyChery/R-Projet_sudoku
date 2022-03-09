cbea91c9be86ad9e11be01de72ae68b92b921569
FannyChery <90441741+FannyChery@users.noreply.github.com>
2022-03-09 19:46:24 +0100

    .

--- R/Tri_possibilite.R
+++ R/Tri_possibilite.R
@@ -1,4 +1,6 @@
-#' Fonction q
+#' Fonction qui prend en entrée une matrice avec, dans les cases vides du sudoku à compléter,
+#' le nombre de possibilités par case. Et qui retourne une matrice qui ordonne par ordre
+#' croissant ces cases.
 #'
 #' @param count_matrice : matrice 9x9 avec dans les cases qui sont à compléter
 #' @return Affiche la matrice avec les ordres des cases triés.
@@ -8,13 +10,13 @@
 #Fonction qui ordonne les cases vides en fonction des nb de possibilité
 tri_possibilite <- function (count_matrice){
   countmat <- matrix(" ", nrow = 9, ncol = 9)
-  compteur = 1
+  compteur <- 1
   for (k in 1:9){
     for (i in 1:9){
       for (j in 1:9){
         if(count_matrice[i,j] == k){
-          countmat[i,j]=compteur
-          compteur = compteur +1
+          countmat[i,j] <- compteur
+          compteur <- compteur +1
         }
       }
     }
