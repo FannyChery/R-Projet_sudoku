16e3bbffa4a858d9fe449c22288d972297f6ac4b
FannyChery <90441741+FannyChery@users.noreply.github.com>
2022-03-10 14:26:02 +0100

    .

--- Sudoku-app/server.R
+++ Sudoku-app/server.R
@@ -1,20 +1,10 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(gridExtra)
 library(shiny)
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
     output$sudoku <- renderPlot({
-      if (input$select == "generer sudoku incomplet"){
+      if (input$select == "Génerer sudoku incomplet"){
         sudoku = cbind(c(9,0,8,0,0,0,2,0,0),c(0,0,0,0,0,0,0,0,0),
                        c(0,5,0,0,0,0,0,0,1), c(1,0,0,0,7,0,3,2,9),
                        c(0,9,4,8,0,2,0,0,6), c(0,0,0,0,0,6,0,0,4),
@@ -25,6 +15,6 @@ shinyServer(function(input, output) {
       }
       grid.table(sudoku)
     })
-    
+
     output$value <- renderPrint({ input$select })
 })
