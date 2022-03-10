3bfee82d1420876a5a83b6d13dd37b7cacd0ea05
Olivier97400 <91317392+Olivier97400@users.noreply.github.com>
2022-03-10 05:04:18 +0100

    Update ui.R
    Affichage d'une grille représentant la matrice de sudoku incomplète ou composée uniquement de zéros (il s'agit d'un test) !

--- Sudoku-app/ui.R
+++ Sudoku-app/ui.R
@@ -20,7 +20,7 @@ shinyUI(fluidPage(
     sidebarPanel(
       selectInput("select", label = h3("Faites votre choix"),
                   choices = list("generer sudoku complet", 
-                                 "genere sudoku incomplet",
+                                 "generer sudoku incomplet",
                                  "resoudre le sudoku")),
     hr(),
     verbatimTextOutput("value")
