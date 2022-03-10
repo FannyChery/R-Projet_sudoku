aa13894e2feb6058fb8e345d7c184a55b926d480
Olivier97400 <91317392+Olivier97400@users.noreply.github.com>
2022-03-10 04:58:38 +0100

    Update server.R
    Affichage d'une grille représentant la matrice de sudoku incomplète ou composée uniquement de zéros (il s'agit d'un test) !

--- Sudoku-app/server.R
+++ Sudoku-app/server.R
@@ -7,20 +7,24 @@
 #    http://shiny.rstudio.com/
 #
 
+library(gridExtra)
 library(shiny)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+    output$sudoku <- renderPlot({
+      if (input$select == "generer sudoku complet"){
+        sudoku = cbind(c(9,0,8,0,0,0,2,0,0),c(0,0,0,0,0,0,0,0,0),
+                       c(0,5,0,0,0,0,0,0,1), c(1,0,0,0,7,0,3,2,9),
+                       c(0,9,4,8,0,2,0,0,6), c(0,0,0,0,0,6,0,0,4),
+                       c(0,2,0,0,0,0,0,9,5), c(0,0,0,0,0,0,0,0,7),
+                       c(5,1,7,0,0,9,6,0,0))
+      } else {
+        sudoku = matrix(0,9,9)
+      }
+      grid.table(sudoku)
     })
-
+    
+    output$value <- renderPrint({ input$select })
 })
