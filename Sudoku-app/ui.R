29fb1c2b6a40279fdd78aa7e2c350416a792eb17
Olivier97400 <91317392+Olivier97400@users.noreply.github.com>
2022-03-11 16:33:44 +0100

    Update ui.R

--- Sudoku-app/ui.R
+++ Sudoku-app/ui.R
@@ -1,33 +1,33 @@
 library(gridExtra)
 library(shiny)
 devtools::load_all()
-
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Sudoku game by Fanny Chery and Olivier COME"),
-
-    # gere la partie gauche de l'app
-    sidebarPanel(
-      selectInput("select", label = h4("Faites votre choix"),
-                  choices = list("initialisation", "Jouer",
-                                 "Donner la solution sans backtracking",
-                                 "resoudre le sudoku avec backtracking"),
-                  selected = ""),
-    hr(),
-    #verbatimTextOutput("value"),
+  
+  # Application title
+  titlePanel("Grille de Sudoku"),
+  
+  # Gere la partie gauche de l'app
+  sidebarPanel(
+    fluidRow(
+      actionButton("button", "jouer")
+    ),
+    
+    fluidRow(
+      actionButton("button", "solution")
+    ),
     
     selectInput("level", label = h4("Niveau de difficulte"),
-                choices = list("", "facile", "moyen", "difficile"),
+                choices = list("", "Facile", "Intermediaire", "difficile",
+                               "Expert"),
                 selected = ""),
     hr(),
-    #verbatimTextOutput("value")
-    ), 
-
-        # gere la partie droite de l'app
-        mainPanel(
-          plotOutput("sudoku")
-        )
-    )
+  ),
+  
+  # gere la partie droite de l'app
+  mainPanel(
+    plotOutput("sudoku"),
+    #plotOutput("solver")
+    
+  )
+)
 )
