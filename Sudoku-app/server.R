c9d16c67a7cc2e4fe7579a6b8bdecf65111246c2
Olivier97400 <91317392+Olivier97400@users.noreply.github.com>
2022-03-11 16:34:22 +0100

    Update server.R

--- Sudoku-app/server.R
+++ Sudoku-app/server.R
@@ -3,23 +3,18 @@ library(shiny)
 
 devtools::load_all()
 
-# Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-    output$sudoku <- renderPlot({
-    if (input$select == "Jouer"){
+  
+  output$sudoku <- renderPlot({
+    if (input$button == "jouer"){
       m = create_Sudoku()
       mat_complete <<- permute(m)
       mat_incomplete <<- SUDOKU(mat_complete)
       sudoku = mat_incomplete
-    } else if (input$select == "resoudre le sudoku avec backtracking"){
-        sudoku = backtraking(mat_incomplete)
-    } else if (input$select == "Donner la solution sans backtracking") {
-      sudoku = mat_complete
     } else {
-      sudoku = matrix(0, 9, 9)
+        sudoku = mat_complete
     }
     grid.table(sudoku)
   })
   
-  #output$value <- renderPrint({ input$select })
 })
