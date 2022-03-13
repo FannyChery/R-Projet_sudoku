f108b421d5a4d66fa896648c899ad1c5bb43266f
Olivier97400 <91317392+Olivier97400@users.noreply.github.com>
2022-03-13 05:32:10 +0100

    Create difficultyLevels.R
    Cette fonction génère une matrice de sudoku incomplète associée à un niveau de difficulté qui est donné en argument.

---
+++ R/difficultyLevels.R
@@ -0,0 +1,43 @@
+generateLevelMat = function(level){
+  n = 0
+  mat = create_Sudoku()
+  mat_complete = permute(mat)
+  if (level == "Facile"){
+    while (n < 1) {
+      easyMat = delete_val(mat_complete, 45)
+      nbOfback = backtraking(easyMat)[1]
+      if (nbOfback < 1000){
+        matLevel = easyMat
+        n = n + 1
+      }
+    }
+    
+  }else if (level == "Intermediaire"){
+    while (n < 1) {
+      middleMat = delete_val(mat_complete, 50)
+      nbOfback = backtraking(middleMat)[1]
+      if ((nbOfback >= 1000) & (nbOfback < 5000)){
+        matLevel = middleMat
+        n = n + 1
+      }
+    }
+  }else if (level == "Difficile"){
+    while (n < 1) {
+      difficultMat = delete_val(mat_complete, 53)
+      nbOfback = backtraking(difficultMat)[1]
+      if ((nbOfback >= 5000) & (nbOfback < 10000)){
+        matLevel = difficultMat
+        n = n + 1
+      }
+    }
+  }else if (level == "Expert"){
+    while (n < 1) {
+      expertMat = delete_val(mat_complete, 55)
+      nbOfback = backtraking(expertMat)[1]
+      if (nbOfback >= 10000){
+        matLevel = expertMat
+        n = n + 1
+      }
+    }
+  }
+  return(matLevel)
