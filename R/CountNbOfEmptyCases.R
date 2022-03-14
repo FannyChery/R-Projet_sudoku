0b1504907514190af532ae7f4191003322473fe3
Olivier97400 <91317392+Olivier97400@users.noreply.github.com>
2022-03-14 15:36:18 +0100

    Update CountNbOfEmptyCases.R

--- R/CountNbOfEmptyCases.R
+++ R/CountNbOfEmptyCases.R
@@ -1,3 +1,8 @@
+#' Fonction qui retourne le nombre de cases vides présentent dans la matrice
+#'
+#' @param matrice : matrice 9x9 avec des cases vides
+#' @return le nombre de cases vides qu'il y a dans la matrice
+
 nbOfZero = function(mat){
   cpt = 0
   for (i in 1:(dim(mat)[1])){
