32054e5c321397db5524f41c2881031dcac9d2a7
Olivier97400 <91317392+Olivier97400@users.noreply.github.com>
2022-03-14 16:16:20 +0100

    Update accuracy.R

--- R/accuracy.R
+++ R/accuracy.R
@@ -1,3 +1,8 @@
+#' Fonction qui retourne la moyenne des backtrackings d'un échantillon de 20 matrices incomplètes non identiques mais ayant le même nombre de cases vides.
+#'
+#' @param matrice : un entier designant le nombre de cases vides de la matrice
+#' @return un nombre qui désigne la moyenne des backtrackings
+
 accuracy = function(emptyCase){
   nb_back = c(rep(0,50))
   for (i in 1:50){
